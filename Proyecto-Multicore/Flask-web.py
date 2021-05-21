995f24b7f95c87563a72d08edfec1f88bf7a7b95
Fernanda Duran <61404000+ferduran06@users.noreply.github.com>
2021-05-21 09:31:51 -0600

    Un poco de documentación y algo de diseño web

--- Proyecto-Multicore/Flask-web.py
+++ Proyecto-Multicore/Flask-web.py
@@ -2,13 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
+
 def home():
-    with open('Proyecto-Multicore\\templates\game_data.txt','r',encoding="utf-8") as file:
+    with open('Proyecto-Multicore\\templates\game_data.txt','r',encoding="utf-8") as file:     #Abre el archivo donde se guarda la información
         l_games = file.readlines()
         l = len(l_games)
 
-        return render_template('home.html',data=l_games, len=l)
+        return render_template('home.html',data=l_games, len=l)         #Nos retorna al html para ver la inf
 
 
 if __name__ == '__main__':
